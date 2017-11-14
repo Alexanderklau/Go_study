18e70b3ee9b06cf121d8449476026fcad01d4ba1
Alexanderklau <429095816@qq.com>
2017-11-14 14:22:09 +0800

    播放器

--- music/mplayer.go
+++ music/mplayer.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"string"
-
+	"strings"
 	"pkg/mplayer/mlib"
 	"pkg/mplayer/mp3"
 )
@@ -20,7 +19,7 @@ func handleLibCommands(tokens []string) {
 	case "list":
 		for i := 0; i < lib.Len(); i++ {
 			e, _ := lib.Get(i)
-			fmt.PrintIn(i+1, ":", e.Name, e.Artist, e.Source, e.Type)
+			fmt.Println(i+1, ":", e.Name, e.Artist, e.Source, e.Type)
 		}
 	case "add":{
 		if len(tokens) == 6 {
@@ -28,7 +27,64 @@ func handleLibCommands(tokens []string) {
 			lib.Add(&library.MusicEntry{strconv.Itoa(id)},
 		    tokens[2], tokens[3], tokens[4], tokens[5]})
 		} else{
-			fmt.PrintIn("USAGE: lib add <name><artist><Source><type>")
+			fmt.Println("USAGE: lib add <name><artist><Source><type>")
 		}
+    }
+    case "remove":{
+        if len(tokens) == 3 {
+            lib.RemoveByName(tokens[2])
+        } else {
+            fmt.Println("USAGE: lib remove <name>")
+        }
+    default:
+        fmt.Println("Unrecognized lib command:", tokens[1])
 	}
+}
+
+func handlePlayCommand(tokens []string) {
+    if len(tokens) != 2 {
+        fmt.Println("USAGE: play<name> ")
+        return
+    }
+
+    e := lib.Find(tokens[1])
+    if e == nil {
+        fmt.Println("The music", tokens[1], "does not exist.")
+        return
+    }
+
+    mp.Play(e.Source, e.Type, ctrl, signal)
+}
+
+func main() {
+    fmt.Println(`
+              Enter following commands to control the player:
+              lib list -- View the existing music lib
+              lib add <name><artist><source><type> -- Add a music to the music lib
+              lib remove <name> -- Remove the specified music from the lib
+              play <name> -- Play the specified music
+              `)
+    lib = library.NewMusicManager()
+
+    r := bufio.NewReader(os.Stdin)
+
+    for {
+        fmt.Print("Enter command--->")
+
+        rawLine, _, _ := r.ReadLine()
+
+        line := string(rawLine)
+
+        if line == "q" || line == "e" {
+            break
+        }
+
+        tokens := strings.Split(line, " ")
+
+        if tokens[0] == "lib" {
+            handleLibCommands(tokens)
+        } else {
+            fmt.Println("Unrecognized command: ", tokens[0])
+        }
+    }
 }
