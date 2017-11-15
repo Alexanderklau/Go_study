f940e172339884538d0554aab7f30fbad18ffc75
Alexanderklau <429095816@qq.com>
2017-11-15 18:26:39 +0800

    orm

--- go-orm/create-table-mysql.go
+++ go-orm/create-table-mysql.go
@@ -5,20 +5,33 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type user struct {
-	uid      int    `gorm:"size:64;not null;unique;primary_key"` //uid值，长度 255, 不为空，唯一值, 主键
-	uname    string `gorm:"size:l40"`
-	password string `gorm:"size:128`
-	status   int    `gorm:"size:4"`
+type User struct {
+	Uid      int     `gorm:"size:64;not null;unique;primary_key"` //uid值，长度 255, 不为空，唯一值, 主键
+	Uname    string  `gorm:"type:varchar(40);not null;unique"`
+	Password string  `gorm:"type:varchar(128)"`
+	Status   int     `gorm:"size:4"`
+	Group    []Group `gorm:"many2many:user_groups"`
+}
+
+type Group struct {
+	Gid    int    `gorm:"size:64;unique;primary_key"`
+	Name   string `gorm:"type:varchar(64);unique"`
+	Status int    `gorm:"size:4"`
+}
+
+type User_group struct {
+	Status int `gorm:"size:4"`
+}
+
+type Group_relation struct {
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:123456@tcp(localhost:3306)/datrix")
+	db, err := gorm.Open("mysql", "root:123456@tcp(localhost:3306)/tests2")
 	if err != nil {
 		panic("failed to connect database")
 	}
 	defer db.Close()
 
-	db.AutoMigrate(&user{})
-
+	db.AutoMigrate(&User{}, &Group{}, &User_group{})
 }
