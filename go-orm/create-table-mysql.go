873b8ccf524dea8ec8193e9e1544d21554caa822
Alexanderklau <429095816@qq.com>
2017-11-16 13:07:21 +0800

    orm

--- go-orm/create-table-mysql.go
+++ go-orm/create-table-mysql.go
@@ -14,9 +14,10 @@ type User struct {
 }
 
 type Group struct {
-	Gid    int    `gorm:"size:64;unique;primary_key"`
-	Name   string `gorm:"type:varchar(64);unique"`
-	Status int    `gorm:"size:4"`
+	Gid             int              `gorm:"size:64;unique;primary_key"`
+	Name            string           `gorm:"type:varchar(64);unique"`
+	Status          int              `gorm:"size:4"`
+	Group_relations []Group_relation `gorm:"ForeignKey:Parent_gid;AssociationForeignKey:Gid"`
 }
 
 type User_group struct {
@@ -24,14 +25,42 @@ type User_group struct {
 }
 
 type Group_relation struct {
+	Gids       int   `gorm:"primary_key"`
+	Groups     Group `gorm:"ForeignKey:Gids;AssociationForeignKey:Gid"`
+	Parent_gid int
+	Status     int
+}
+
+type Group_relation_log struct {
+	Op_id    string `gorm:"type:varchar(64)"`
+	Op_type  string `gorm:"type:varchar(32)"`
+	Op_value string `gorm:"type:varchar(255)"`
+}
+
+type Group_log struct {
+	Op_id    string `gorm:"type:varchar(64)"`
+	Op_type  string `gorm:"type:varchar(32)"`
+	Op_value string `gorm:"type:varchar(255)"`
+}
+
+type User_group_log struct {
+	Op_id    string `gorm:"type:varchar(64)"`
+	Op_type  string `gorm:"type:varchar(32)"`
+	Op_value string `gorm:"type:varchar(255)"`
+}
+
+type User_log struct {
+	Op_id    string `gorm:"type:varchar(64)"`
+	Op_type  string `gorm:"type:varchar(32)"`
+	Op_value string `gorm:"type:varchar(255)"`
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:123456@tcp(localhost:3306)/tests2")
+	db, err := gorm.Open("mysql", "root:123456@tcp(localhost:3306)/datrix2")
 	if err != nil {
 		panic("failed to connect database")
 	}
 	defer db.Close()
 
-	db.AutoMigrate(&User{}, &Group{}, &User_group{})
+	db.AutoMigrate(&User{}, &Group{}, &User_group{}, &Group_relation{}, &Group_relation_log{}, &Group_log{}, &User_group_log{}, &User_log{})
 }
