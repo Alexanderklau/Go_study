bf5f84020dfd637cb68975cbca8001526fdd1ab3
Alexanderklau <429095816@qq.com>
2017-12-05 15:48:51 +0800

    fix

--- go-web-project/proxy/proxy.go
+++ go-web-project/proxy/proxy.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	// "strings"
 )
 
 type handle struct {
@@ -12,13 +13,45 @@ type handle struct {
 	port string
 }
 
+func newReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.Host = target.Host
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		// req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		// if targetQuery == "" || req.URL.RawQuery == "" {
+		// 	req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		// } else {
+		// 	req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		// }
+		// if _, ok := req.Header["User-Agent"]; !ok {
+		// 	// explicitly disable User-Agent so it's not set to defaul t value
+		// 	req.Header.Set("User-Agent", "")
+		// }
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+// func singleJoiningSlash(a, b string) string {
+// 	aslash := strings.HasSuffix(a, "/")
+// 	bslash := strings.HasPrefix(b, "/")
+// 	switch {
+// 	case aslash && bslash:
+// 		return a + b[1:]
+// 	case !aslash && !bslash:
+// 		return a + "/" + b
+// 	}
+// 	return a + b
+// }
+
 func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	remote, err := url.Parse("http://" + this.host + ":" + this.port)
 	log.Println(remote)
 	if err != nil {
 		panic(err)
 	}
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy := newReverseProxy(remote)
 	proxy.ServeHTTP(w, r)
 }
 
