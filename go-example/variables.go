1ee74c59b744d67047e6754158c6be63bc8bd0bb
Alexanderklau <429095816@qq.com>
2017-12-12 11:05:03 +0800

    fix

---
+++ go-example/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := "internal"
+	fmt.Println(a)
+
+	b, c := 1, 2
+	fmt.Println(b, c)
+
+	d := true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
